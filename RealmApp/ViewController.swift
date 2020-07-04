3de05463e3971c3836388d1206e4ae18d0d4e454
Egor Ukolov <ukolovegor@yandex.ru>
2020-07-04 21:16:07 +0700

    Initial Commit

---
+++ RealmApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmApp
+//
+//  Created by Egor Ukolov on 04.07.2020.
+//  Copyright © 2020 Egor Ukolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
